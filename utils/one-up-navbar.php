6618850362422c0403e444b2e6adea804cf7fbd6
MrMiester <sethpommer@yahoo.com>
2023-11-01 19:22:15 -0500

    Added Adeptus Custodes and Adeptus Mechanicus

--- utils/one-up-navbar.php
+++ utils/one-up-navbar.php
@@ -25,6 +25,8 @@ echo <<< END
                             <div class="dropdown-content">
                               <a href="../points/votann-points.php">Leagues of Votann</a>
                               <a href="../points/sororitas-points.php">Adepta Sororitas</a>
+                              <a href="../points/custodes-points.php">Adeptus Custodes</a>
+                              <a href="../points/mechanicus-points.php">Adeptus Mechanicus</a>
                             </div>
                         </div>
                     </li>
