952ae031b0af95d76e29d5c3bc62d14bb395d52e
dreadvisage <tysobojinski@gmail.com>
2023-11-14 21:50:07 -0600

    the three line menu btn toggles the menu as well

--- webroot/js/alt-navbar-three-line-menu.js
+++ webroot/js/alt-navbar-three-line-menu.js
@@ -14,12 +14,12 @@ function closeThreeLineMenu() {
 }
 
 /* This does two things. For every element in the document, add a listener that will close
-the three line menu. Except for the three line menu image itself, when clicking that, the 
-menu will open/close.
+the three line menu. Except for the three line menu image itself and the associated button, 
+when clicking that, the menu will open/close.
 */
 function registerThreeLineMenuListeners() {
     document.querySelectorAll("*").forEach(item => {
-        if (item.className == "three-line-icon") {
+        if (item.className == "three-line-icon" || item.id == "three-line-menu-btn") {
             item.addEventListener("click", event => {
                 event.stopPropagation();
                 toggleThreeLineMenu();
