e091054480dd336f8f93e763f946a106ab12b3ea
dreadvisage <tysobojinski@gmail.com>
2023-11-29 16:48:17 -0600

    added more main html tags to more webpages

--- webroot/faq.php
+++ webroot/faq.php
@@ -1,10 +1,11 @@
 <?php
 session_start();
-require '../utils/navbar.php'; 
+require '../utils/navbar.php';
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,109 +13,112 @@ require '../utils/navbar.php';
     <link rel="stylesheet" href="css/navbar.css">
     <link rel="stylesheet" href="css/article-section.css">
 </head>
-<body>
-<div id="background-image"><img src="images/warhammer-background.jpg" alt="Warhammer Background"></div>
 
-    <?php 
-        echoNavbar(0);
+<body>
+    <div id="background-image"><img src="images/warhammer-background.jpg" alt="Warhammer Background"></div>
+
+    <?php
+    echoNavbar(0);
     ?>
 
-    <div class="display">
-        <div class="pad"></div>
-        <div id="article"> 
-            
-            <h1>Frequently Asked Questions</h1>
+    <main>
+        <div class="display">
+            <div class="pad"></div>
+            <div id="article">
 
-            <p>
-                Welcome to the Factions main page. Click on your desired faction to be taken to the respective faction page.
-            </p>
+                <h1>Frequently Asked Questions</h1>
 
-            <br>
-            <a href="factions/adepta-sororitas.php">Adepta Sororitas</a>
+                <p>
+                    Welcome to the Factions main page. Click on your desired faction to be taken to the respective faction page.
+                </p>
 
-            <br><br>
-            <a href="factions/adeptus-custodes.php">Adeptus Custodes</a>
+                <br>
+                <a href="factions/adepta-sororitas.php">Adepta Sororitas</a>
 
-            <br><br>
-            <a href="factions/adeptus-mechanicus.php">Adeptus Mechanicus</a>
+                <br><br>
+                <a href="factions/adeptus-custodes.php">Adeptus Custodes</a>
 
-            <br><br>
-            <a href="factions/adeptus-titanicus.php">Adeptus Titanicus</a>
+                <br><br>
+                <a href="factions/adeptus-mechanicus.php">Adeptus Mechanicus</a>
 
-            <br><br>
-            <a href="factions/aeldari.php">Aeldari</a>
+                <br><br>
+                <a href="factions/adeptus-titanicus.php">Adeptus Titanicus</a>
 
-            <br><br>
-            <a href="factions/agents-of-imperium.php">Agents of the Imperium</a>
+                <br><br>
+                <a href="factions/aeldari.php">Aeldari</a>
 
-            <br><br>
-            <a href="factions/astra-militarum.php">Astra Militarum</a>
+                <br><br>
+                <a href="factions/agents-of-imperium.php">Agents of the Imperium</a>
 
-            <br><br>
-            <a href="factions/black-templars.php">Black Templars</a>
+                <br><br>
+                <a href="factions/astra-militarum.php">Astra Militarum</a>
 
-            <br><br>
-            <a href="factions/blood-angels.php">Blood Angels</a>
+                <br><br>
+                <a href="factions/black-templars.php">Black Templars</a>
 
-            <br><br>
-            <a href="factions/chaos-daemons.php">Chaos Daemons</a>
+                <br><br>
+                <a href="factions/blood-angels.php">Blood Angels</a>
 
-            <br><br>
-            <a href="factions/chaos-knights.php">Chaos Knights</a>
+                <br><br>
+                <a href="factions/chaos-daemons.php">Chaos Daemons</a>
 
-            <br><br>
-            <a href="factions/chaos-marines.php">Chaos Space Marines</a>
+                <br><br>
+                <a href="factions/chaos-knights.php">Chaos Knights</a>
 
-            <br><br>
-            <a href="factions/dark-angels.php">Dark Angels</a>
+                <br><br>
+                <a href="factions/chaos-marines.php">Chaos Space Marines</a>
 
-            <br><br>
-            <a href="factions/death-guard.php">Death Guard</a>
+                <br><br>
+                <a href="factions/dark-angels.php">Dark Angels</a>
 
-            <br><br>
-            <a href="factions/deathwatch.php">Deathwatch</a>
+                <br><br>
+                <a href="factions/death-guard.php">Death Guard</a>
 
-            <br><br>
-            <a href="factions/drukhari.php">Drukhari</a>
+                <br><br>
+                <a href="factions/deathwatch.php">Deathwatch</a>
 
-            <br><br>
-            <a href="factions/genestealer-cults.php">Genestealer Cults</a>
+                <br><br>
+                <a href="factions/drukhari.php">Drukhari</a>
 
-            <br><br>
-            <a href="factions/grey-knights.php">Grey Knights</a>
+                <br><br>
+                <a href="factions/genestealer-cults.php">Genestealer Cults</a>
 
-            <br><br>
-            <a href="factions/imperial-knights.php">Imperial Knights</a>
+                <br><br>
+                <a href="factions/grey-knights.php">Grey Knights</a>
 
-            <br><br>
-            <a href="factions/leagues-of-votann.php">Leauges of Votann</a>
+                <br><br>
+                <a href="factions/imperial-knights.php">Imperial Knights</a>
 
-            <br><br>
-            <a href="factions/necrons.php">Necrons</a>
+                <br><br>
+                <a href="factions/leagues-of-votann.php">Leauges of Votann</a>
 
-            <br><br>
-            <a href="factions/orks.php">Orks<a>
+                <br><br>
+                <a href="factions/necrons.php">Necrons</a>
 
-            <br><br>
-            <a href="factions/space-marine.php">Space Marines</a>
+                <br><br>
+                <a href="factions/orks.php">Orks<a>
 
-            <br><br>
-            <a href="factions/space-wolves.php">Space Wolves</a>
+                        <br><br>
+                        <a href="factions/space-marine.php">Space Marines</a>
 
-            <br><br>
-            <a href="factions/t'au-empire.php">T'au Empire</a>
+                        <br><br>
+                        <a href="factions/space-wolves.php">Space Wolves</a>
 
-            <br><br>
-            <a href="factions/thousand-sons.php">Thousand Sons</a>
+                        <br><br>
+                        <a href="factions/t'au-empire.php">T'au Empire</a>
 
-            <br><br>
-            <a href="factions/tyranids.php">Tyranids</a>
+                        <br><br>
+                        <a href="factions/thousand-sons.php">Thousand Sons</a>
 
-            <br><br>
-            <a href="factions/world-eaters.php">World Eaters</a>
+                        <br><br>
+                        <a href="factions/tyranids.php">Tyranids</a>
+
+                        <br><br>
+                        <a href="factions/world-eaters.php">World Eaters</a>
+            </div>
+            <div class="pad"></div>
         </div>
-        <div class="pad"></div>
-    </div>    
+    </main>
 
     <footer>
         <div class="pad"></div>
@@ -125,4 +129,5 @@ require '../utils/navbar.php';
         <div class="pad"></div>
     </footer>
 </body>
+
 </html>
--- webroot/indexes.php
+++ webroot/indexes.php
@@ -1,10 +1,11 @@
 <?php
 session_start();
-require '../utils/navbar.php'; 
+require '../utils/navbar.php';
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,20 +13,23 @@ require '../utils/navbar.php';
     <link rel="stylesheet" href="css/navbar.css">
     <link rel="stylesheet" href="css/article-section.css">
 </head>
+
 <body>
     <div id="background-image"><img src="images/warhammer-background.jpg" alt="Warhammer Background"></div>
 
-    <?php 
-        echoNavbar(0);
+    <?php
+    echoNavbar(0);
     ?>
 
-    <div class="display">
-        <div class="pad"></div>
-        <div id="article"> 
-            <p>This is the indexes page!</p>
+    <main>
+        <div class="display">
+            <div class="pad"></div>
+            <div id="article">
+                <p>This is the indexes page!</p>
+            </div>
+            <div class="pad"></div>
         </div>
-        <div class="pad"></div>
-    </div>    
+    </main>
 
     <footer>
         <div class="pad"></div>
@@ -36,4 +40,5 @@ require '../utils/navbar.php';
         <div class="pad"></div>
     </footer>
 </body>
+
 </html>
